aefea3d22649deb02b804dd7c086ff823bcd40c9
wonjunghee <wjdgml0078@naver.com>
2021-05-29 16:03:27 +0900

    Updates

---
+++ precache-manifest.11acceb4df56d2f7a0492b1005ed0ef8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3dd15ef258964fa15b015b2375bdd2a",
+    "url": "/account_book/index.html"
+  },
+  {
+    "revision": "9f37ef837778da2c70c2",
+    "url": "/account_book/static/css/main.e9a91d9e.chunk.css"
+  },
+  {
+    "revision": "6f543ee136af79495204",
+    "url": "/account_book/static/js/2.40dd7955.chunk.js"
+  },
+  {
+    "revision": "e5051ff34247a5ecf981d18b5b70f9bc",
+    "url": "/account_book/static/js/2.40dd7955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f37ef837778da2c70c2",
+    "url": "/account_book/static/js/main.6eb3b61a.chunk.js"
+  },
+  {
+    "revision": "8ed3c70638b1413f16d2",
+    "url": "/account_book/static/js/runtime-main.38e8c42c.js"
+  },
+  {
+    "revision": "46f5c72597e0f0b0f06d67f32cd6fac8",
+    "url": "/account_book/static/media/AppIcon.46f5c725.png"
+  },
+  {
+    "revision": "a54ee73ed71eff1d6d041115dbcf63b8",
+    "url": "/account_book/static/media/IPhone6.a54ee73e.png"
+  },
+  {
+    "revision": "ca30193858d252cff03718ebe6d70680",
+    "url": "/account_book/static/media/WallPaper.ca301938.png"
+  }
+]);
